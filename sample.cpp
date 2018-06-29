50f9d36103dba28ca5458bb7e380f0c74c6e8123
Rakesh V <v.rakesh@nokia.com>
2018-06-29 15:49:15 +0530

    second commit on main folder's sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -2,7 +2,9 @@
 
 int main()
 {
-    std::cout << "Test sub Branch" << std::endl;
+    std::cout << "Test main Branch" << std::endl;
+
+    std::cout << "Test main Branch 1" << std::endl;
 
     return 0;
 }
